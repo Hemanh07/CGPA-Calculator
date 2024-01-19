b922287f577de6984467f624f96e239388dc6821
Hemanth07 <143334855+Hemanh07@users.noreply.github.com>
2024-01-19 13:24:57 +0530

    Upd

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ calculate.addEventListener("click",(event)=>{
     result.innerHTML=total.toFixed(3);
 },false);
 let cal=function(score){
-    score=(score*100)/60;
+    score=(score*100)/(6*10);
     return score/10 ;
 }
